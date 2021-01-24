43271502c0b24af9ce7eb5dc2b40a08a30fe1239
layla  hammoud <laylahammoud63@gmail.com>
2021-01-24 14:24:35 +0000

    Automatically backed up by Learn

--- src/components/LoginForm.js
+++ src/components/LoginForm.js
@@ -4,7 +4,10 @@ class LoginForm extends React.Component {
   constructor() {
     super();
 
-    this.state = {};
+    this.state = {
+      username:""
+      password:""
+    };
   }
 
   render() {
@@ -13,13 +16,13 @@ class LoginForm extends React.Component {
         <div>
           <label>
             Username
-            <input id="username" name="username" type="text" />
+            <input id="username" name="username" type="text" value={this.state.username} />
           </label>
         </div>
         <div>
           <label>
             Password
-            <input id="password" name="password" type="password" />
+            <input id="password" name="password" type="password" valur={this.state.password} />
           </label>
         </div>
         <div>
