172a2a923e1cc7d08543d32dab0aaa691836450f
layla  hammoud <laylahammoud63@gmail.com>
2021-01-24 14:22:41 +0000

    Automatically backed up by Learn

--- src/components/TwitterMessage.js
+++ src/components/TwitterMessage.js
@@ -6,12 +6,19 @@ class TwitterMessage extends React.Component {
 
     this.state = {};
   }
+  const handleInputChange=()=>{
+      this.state({
+      [event.target.name]=event.target.value 
+        
+    })
+      
+  }
 
   render() {
     return (
       <div>
         <strong>Your message:</strong>
-        <input type="text" name="message" id="message" />
+        <input type="text" name="message" id="message" onchange={event=>this.handleInputChange(event)}/>
       </div>
     );
   }
